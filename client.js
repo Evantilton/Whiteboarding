0ec3a48b52a18b6f3f1da4f31be0e031bc5ecf1d
evantilton <evantilton@gmail.com>
2020-08-10 13:34:57 -0500

    problem 1 finished

--- client.js
+++ client.js
@@ -13,11 +13,12 @@
 // Notes
 // If you get stuck on a challenge, find help in the Resources tab.
 // If you're really stuck, unlock solutions in the Solutions tab.
-Console.log("hello world")
+console.log("hello world")
 
 function countWords(str) {
 	let count = str.split(" ");
-	return count.length;
+	return console.log(count.length);
 }
 
 countWords("Just an example here move along");
+
