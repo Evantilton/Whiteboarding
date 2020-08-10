9b4b28db147b1c7e7769f323f5d8465ef67c6c0a
evantilton <evantilton@gmail.com>
2020-08-10 13:32:24 -0500

    added a challenge

--- client.js
+++ client.js
@@ -1,1 +1,23 @@
-hello
+//All of these are practice taken from EDABIT.COM
+
+// Get Word Count
+// Create a function that takes a string and returns the word count.
+//  The string will be a sentence.
+
+// Examples
+// countWords("Just an example here move along") ➞ 6
+
+// countWords("This is a test") ➞ 4
+
+// countWords("What an easy task, right") ➞ 5
+// Notes
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+Console.log("hello world")
+
+function countWords(str) {
+	let count = str.split(" ");
+	return count.length;
+}
+
+countWords("Just an example here move along");
