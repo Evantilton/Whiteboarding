80ea6d5ce24f96408e180de71e62b8541627c558
evantilton <evantilton@gmail.com>
2020-08-22 13:10:49 -0500

    given an array, find the sume of the integers

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
-//All of these are practice taken from EDABIT.COM
+//All of these are practice taken from EDABIT.COM unless they say Hackerrank.com
+//Run with the command: node client.js
 
 console.log("hello world")
 
@@ -16,7 +17,8 @@ console.log("hello world")
 // Notes
 // If you get stuck on a challenge, find help in the Resources tab.
 // If you're really stuck, unlock solutions in the Solutions tab.
-
+/////////////////////
+//This function uses the str.split operator to split the array on the chosen spot"
 function countWords(str) {
 	let count = str.split(" ");
 	return console.log(count.length);
@@ -34,8 +36,8 @@ countWords("Just an example here move along");
 
 // hasKey({ pot: 1, tot: 2, not: 3 }, "not") ➞ true
 
-
-
+///////////////////
+// This uses the in operator to find see if a key is within an object
 function hasKey(obj, key) {
 	return console.log(key in obj);
 }
@@ -44,4 +46,16 @@ hasKey({ a: 44, b: 45, c: 46 }, "d")
 hasKey({ pot: 1, tot: 2, not: 3 }, "not")
 
 /////////////////////////////////////////////////
-// Problem 3
+// Problem 3  (From HackerRank)
+//PROBLEM
+//Given an array of integers, find the sum of its elements.
+
+function simpleArraySum(ar) {
+    let total = 0;
+    for (let i=0; i < ar.length;i++){
+        total = total + ar[i];
+    }
+    return console.log(total);
+}
+
+simpleArraySum([1,2,3,4,10,11]);
