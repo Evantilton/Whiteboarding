acc5ebce9bf3f3765f6f278eba7daecd11c1d512
evantilton <evantilton@gmail.com>
2020-08-22 14:20:00 -0500

    very large sum

--- client.js
+++ client.js
@@ -87,3 +87,29 @@ function compareTriplets(a, b) {
     return console.log([alice,bob]);
 }
 compareTriplets([5,6,7],[3,6,10]);
+// /////////////////
+// Problem 5 (hackerank)
+// In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+// Function Description
+
+// Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+// aVeryBigSum has the following parameter(s):
+
+// int ar[n]: an array of integers .
+// Return
+
+// int: the sum of all array elements
+
+function aVeryBigSum(ar) {
+    let sum = 0;
+    for (let i = 0; i < ar.length; i++) {
+        sum = sum + ar[i];
+    }
+    return console.log(sum);
+}
+
+aVeryBigSum([1000000001,1000000002,1000000003,1000000004,1000000005]);
+
+/////////////////////////////
